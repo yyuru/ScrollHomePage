a212e313bc1f2f1fefe84ce2acbce7ec73a30e7d
nebulas <1198398580@qq.com>
2018-05-25 11:16:56 +0800

    fix bug

--- scrollpage/js/main.js
+++ scrollpage/js/main.js
@@ -1,21 +1,29 @@
 const scrollPage = $("#scrollPage");
 const nowPage = $("#nowPage");
+const proBar=$("#progress-bar")
 var index = 0;
 canScroll = true;
 document.onmousewheel = function(e) {
   if (canScroll) {
+    
     canScroll = false;
     if (e.wheelDelta > 0 && index >= 0) {
       if (index > 0) {
         index--;
+        
       }
       $(scrollPage[0].children[index]).removeClass("active");
 
-      $(scrollPage[0].children[index]).addClass("active");
+      
+    }else if(e.wheelDelta < 0 && index < 3){
+     
 
+     
+      $(scrollPage[0].children[index]).addClass("active");
       index++;
     }
     nowPage.css("margin-top", "-" + index * 72 + "px");
+    
     setTimeout(function() {
       canScroll = true;
     }, 2000);
